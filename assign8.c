04f3dfd4207f413056ce7852b7df64d4357f25de
Michael Chapa <michaelchapa1@gmail.com>
2019-11-27 11:51:45 -0600

    Added info to assign8.c

--- assign8.c
+++ assign8.c
@@ -7,6 +7,8 @@ int main(){
     // Look at fxn execl("/bin/ls", "ls", "-1", NULL); to handle 10 arguments
     // USE execvp(...) to handle your p(?) arguments by looking at $PATH to get
     //  various paths to bash commands (ls, kill, gcc, find, sed, awk, ... etc)
-    
+
+    // Once you execl the program (fork it) you relenquish command to it. 
+
     return 0;
 }
