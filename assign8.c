92b2901169e4dbbfb47c15d2d216f87de012c2a9
Michael Chapa <michaelchapa1@gmail.com>
2019-12-01 17:30:43 -0600

    Got the arguments into an array

--- assign8.c
+++ assign8.c
@@ -10,13 +10,25 @@ int main(int argc, char *argv[]){
 
     // Once you execl the program (fork it) you relenquish command to it. 
 
-    char argBuffer[100][100]; // [0][0] = cmd name, [0][0+] = cmd args
-    int i, j; // used for row and column
+    char *execArgs[30][100];    
+    int i;
+    int j = 0; // used for row and column
+    int rows = 0, column; // get size of commands and arguments
+
     for(i = 1; i < argc; i++)
     {
-        printf("%s\n", argv[i]);
+        if(strcmp(argv[i], ",") == 0)
+        {
+            j+= 1;
+        }
+        else
+        {
+            execArgs[j][i] = argv[i];
+        }
     }
 
     printf("argc: %d\n", argc);
+    printf("ROWS: %d\n", j);
+
     return 0;
 }
---
+++ runit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gcc -o assign8 assign8.c
+assign8 ls -la , ps -fu , kill
